9cc2c538355574d53926a4c6f796556ba9aefb53
Aryan Goyal <goyal.aryan@gmail.com>
2026-09-24 21:45:35 +0530

    [FIX] particular coordinates for placing the pieces [FEAT] add logic for king movement
    Signed-off-by: Aryan Goyal <goyal.aryan@gmail.com>

--- game design/gameImage.py
+++ game design/gameImage.py
@@ -1,12 +1,14 @@
 import pygame
 from characters import pieces
+from rulesPiece import kingMove
 pygame.init()
 
 def handlePiecesAdding(path,x,y):
-    piece = pygame.image.load(path)
+    piece = pygame.image.load(path).convert_alpha()
     pieceRect = piece.get_rect()
     width = pieceRect.width/2
     height = pieceRect.height/2
+    print(width,height)
     piece = pygame.transform.scale(piece,(width,height))
     gameDisplay.blit(piece,(x,y))
 
@@ -15,6 +17,7 @@ backGround = pygame.image.load("images/chessboard.bmp")
 bgRect = backGround.get_rect()
 width = bgRect.width/1.5
 height = bgRect.height/1.5
+print(width,height)
 backGround = pygame.transform.scale(backGround,(width,height)) #? This is the steps that affects the main image
 gameDisplay = pygame.display.set_mode((width,height)) #? This is a step that affects the main display
 
@@ -34,8 +37,14 @@ while run:
             for items in pieces:
                 if(event.pos[0] >= items.x and event.pos[0] <= (items.x+78) and event.pos[1] >= items.y and event.pos[1] <= (items.y+78)):
                     # print(items.name)
-                    pygame.draw.rect(gameDisplay, (105,146,62), pygame.Rect(items.x-7, items.y-3, items.size, items.size)) 
+                    pygame.draw.rect(gameDisplay, (105,146,62), pygame.Rect(items.x, items.y, items.size, items.size)) 
                     handlePiecesAdding(items.url,items.x,items.y)
+                    if(items.type == "king"):
+                        coord = kingMove(items)
+                        print(coord)
+                        for pts in coord:
+                            handlePiecesAdding("images/main_pieces/legal_move_dots.bmp",pts[0],pts[1])
+                        pass
                 else:
                     handlePiecesAdding(items.url,items.x,items.y)
             pygame.display.flip()
--- game design/rulesPiece.py
+++ game design/rulesPiece.py
@@ -5,3 +5,11 @@
 def kingMove(piece):
     curr_x = piece.x
     curr_y = piece.y
+    dx = [78,78,78,-78,-78,-78,0,0]
+    dy = [0,78,-78,0,78,-78,78,-78]
+    ans = []
+    for i in range(8):
+        if(curr_x+dx[i] >= 78 and curr_x+dx[i] <= 621 and curr_y+dy[i]>= 78 and curr_y+dy[i]<=621):
+            ans.append([curr_x+dx[i],curr_y+dy[i]])
+    return ans
+
